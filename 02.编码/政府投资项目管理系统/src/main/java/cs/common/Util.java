e934df08b139bc6f31a9da19e7143aa576eb5302
Jack <1345913545@qq.com>
2017-05-04 20:51:04 +0800

    upload

--- 02.编码/政府投资项目管理系统/src/main/java/cs/common/Util.java
+++ 02.编码/政府投资项目管理系统/src/main/java/cs/common/Util.java
@@ -7,5 +7,7 @@ public class Util {
 	public static String formatDate(Date date) {
 		return new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(date);
 	}
-	
+	public static String generateFileName(String fileName) {
+		return (new SimpleDateFormat("yyyyMMdd_HHmmssS").format(new Date()))+"_"+fileName;
+	}
 }
--- 02.编码/政府投资项目管理系统/src/main/java/cs/controller/framework/DemoController.java
+++ 02.编码/政府投资项目管理系统/src/main/java/cs/controller/framework/DemoController.java
@@ -1,20 +1,44 @@
 package cs.controller.framework;
 
+import java.io.File;
+
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import cs.common.Util;
 
 @Controller
 @RequestMapping(name = "Demo", path = "demo")
 public class DemoController {
 	private String ctrlName = "framework/demo";
 	
+	@Autowired  
+    private HttpServletRequest request;  
 	//@RequiresPermissions("demo#save#post")
 	@RequestMapping(name = "上传文件", path = "save", method = RequestMethod.POST)
-	public @ResponseBody String Save(HttpServletRequest request){
+	public @ResponseBody String Save(@RequestParam("file") MultipartFile file){
+		if (!file.isEmpty()) {  
+            try {  
+            	//文件名：
+            	String fileName=file.getOriginalFilename();
+            	//随机名
+            	String randomName=Util.generateFileName(fileName);
+                // 文件保存路径  
+                String filePath = request.getSession().getServletContext().getRealPath("/") + "contents/upload/"  
+                        + randomName;  
+                // 转存文件  
+                file.transferTo(new File(filePath));  
+            } catch (Exception e) {  
+                e.printStackTrace();  
+            }  
+        }  
 		return "true";
 	}
 	@RequestMapping(name = "删除上传文件", path = "remove", method = RequestMethod.POST)
