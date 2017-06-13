dfb589f3f0ef016c82ea2a05b0565f285602bc4a
Jack <1345913545@qq.com>
2017-06-13 14:18:41 +0800

    年度计划

--- 02.编码/政府投资项目管理系统/src/main/java/cs/domain/ShenBaoInfo.java
+++ 02.编码/政府投资项目管理系统/src/main/java/cs/domain/ShenBaoInfo.java
@@ -32,10 +32,65 @@ public class ShenBaoInfo extends BaseProject{
 	private Double applyYearInvest;	
 	//end#年度计划相关
 	
+	//begin#关联信息
 	@OneToOne
 	private ShenBaoUnitInfo bianZhiUnitInfo=new ShenBaoUnitInfo();
 	@OneToOne
 	private ShenBaoUnitInfo shenBaoUnitInfo=new ShenBaoUnitInfo();
 	@OneToMany(cascade=CascadeType.ALL)
 	private List<Attachment> attachments=new ArrayList<>();
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getProjectId() {
+		return projectId;
+	}
+	public void setProjectId(String projectId) {
+		this.projectId = projectId;
+	}
+	public String getProjectShenBaoStage() {
+		return projectShenBaoStage;
+	}
+	public void setProjectShenBaoStage(String projectShenBaoStage) {
+		this.projectShenBaoStage = projectShenBaoStage;
+	}
+	public String getProjectConstrChar() {
+		return projectConstrChar;
+	}
+	public void setProjectConstrChar(String projectConstrChar) {
+		this.projectConstrChar = projectConstrChar;
+	}
+	public Integer getPlanYear() {
+		return planYear;
+	}
+	public void setPlanYear(Integer planYear) {
+		this.planYear = planYear;
+	}
+	public Double getApplyYearInvest() {
+		return applyYearInvest;
+	}
+	public void setApplyYearInvest(Double applyYearInvest) {
+		this.applyYearInvest = applyYearInvest;
+	}
+	public ShenBaoUnitInfo getBianZhiUnitInfo() {
+		return bianZhiUnitInfo;
+	}
+	public void setBianZhiUnitInfo(ShenBaoUnitInfo bianZhiUnitInfo) {
+		this.bianZhiUnitInfo = bianZhiUnitInfo;
+	}
+	public ShenBaoUnitInfo getShenBaoUnitInfo() {
+		return shenBaoUnitInfo;
+	}
+	public void setShenBaoUnitInfo(ShenBaoUnitInfo shenBaoUnitInfo) {
+		this.shenBaoUnitInfo = shenBaoUnitInfo;
+	}
+	public List<Attachment> getAttachments() {
+		return attachments;
+	}
+	public void setAttachments(List<Attachment> attachments) {
+		this.attachments = attachments;
+	}
 }
--- 02.编码/政府投资项目管理系统/src/main/java/cs/model/DtoMapper/ShenBaoInfoMapper.java
+++ 02.编码/政府投资项目管理系统/src/main/java/cs/model/DtoMapper/ShenBaoInfoMapper.java
@@ -13,27 +13,32 @@ public class ShenBaoInfoMapper implements IMapper<ShenBaoInfoDto, ShenBaoInfo> {
 	public ShenBaoInfoDto toDto(ShenBaoInfo entity) {
 		ShenBaoInfoDto shenBaoInfoDto=new ShenBaoInfoDto();
 		if(shenBaoInfoDto!=null){			
-			shenBaoInfoDto.setCapitalQCZ_gtzj(entity.getCapitalQCZ_gtzj());
 			shenBaoInfoDto.setProjectName(entity.getProjectName());
+			shenBaoInfoDto.setCapitalQCZ_gtzj(entity.getCapitalQCZ_gtzj());
 			shenBaoInfoDto.setCapitalSCZ_gtzj(entity.getCapitalSCZ_gtzj());
-			shenBaoInfoDto.setPifuCBSJYGS_date(entity.getPifuCBSJYGS_date());
+			shenBaoInfoDto.setPifuCBSJYGS_date(entity.getPifuCBSJYGS_date());	
 			shenBaoInfoDto.setModifiedDate(entity.getModifiedDate());
 			shenBaoInfoDto.setModifiedBy(entity.getModifiedBy());
+			shenBaoInfoDto.setProjectConstrChar(entity.getProjectConstrChar());			
 			shenBaoInfoDto.setCapitalSCZ_zxzj(entity.getCapitalSCZ_zxzj());
 			shenBaoInfoDto.setCapitalSHTZ(entity.getCapitalSHTZ());
 			shenBaoInfoDto.setRemark(entity.getRemark());
+			shenBaoInfoDto.setProjectShenBaoStage(entity.getProjectShenBaoStage());
 			shenBaoInfoDto.setCapitalOther(entity.getCapitalOther());
 			shenBaoInfoDto.setProjectInvestSum(entity.getProjectInvestSum());
-			shenBaoInfoDto.setProjectAddress(entity.getProjectAddress());
+			shenBaoInfoDto.setApplyYearInvest(entity.getApplyYearInvest());
+			shenBaoInfoDto.setProjectAddress(entity.getProjectAddress());		
 			shenBaoInfoDto.setPifuJYS_date(entity.getPifuJYS_date());
+			shenBaoInfoDto.setPlanYear(entity.getPlanYear());
 			shenBaoInfoDto.setCapitalQCZ_ggys(entity.getCapitalQCZ_ggys());
 			shenBaoInfoDto.setProjectIntro(entity.getProjectIntro());
 			shenBaoInfoDto.setCreatedBy(entity.getCreatedBy());
 			shenBaoInfoDto.setProjectNumber(entity.getProjectNumber());
 			shenBaoInfoDto.setProjectStage(entity.getProjectStage());
+			shenBaoInfoDto.setProjectId(entity.getProjectId());
 			shenBaoInfoDto.setProjectClassify(entity.getProjectClassify());
-			shenBaoInfoDto.setPifuKXXYJBG_wenhao(entity.getPifuKXXYJBG_wenhao());
 			shenBaoInfoDto.setUnitName(entity.getUnitName());
+			shenBaoInfoDto.setPifuKXXYJBG_wenhao(entity.getPifuKXXYJBG_wenhao());
 			shenBaoInfoDto.setItemOrder(entity.getItemOrder());
 			shenBaoInfoDto.setCapitalSCZ_ggys(entity.getCapitalSCZ_ggys());
 			shenBaoInfoDto.setEndDate(entity.getEndDate());
@@ -43,9 +48,11 @@ public class ShenBaoInfoMapper implements IMapper<ShenBaoInfoDto, ShenBaoInfo> {
 			shenBaoInfoDto.setProjectType(entity.getProjectType());
 			shenBaoInfoDto.setCreatedDate(entity.getCreatedDate());
 			shenBaoInfoDto.setCapitalOtherDescription(entity.getCapitalOtherDescription());
+			shenBaoInfoDto.setId(entity.getId());
 			shenBaoInfoDto.setPifuJYS_wenhao(entity.getPifuJYS_wenhao());
-			shenBaoInfoDto.setPifuKXXYJBG_date(entity.getPifuKXXYJBG_date());
 			shenBaoInfoDto.setProjectGuiMo(entity.getProjectGuiMo());
+			shenBaoInfoDto.setPifuKXXYJBG_date(entity.getPifuKXXYJBG_date());
+
 
 		}
 		return  shenBaoInfoDto;
