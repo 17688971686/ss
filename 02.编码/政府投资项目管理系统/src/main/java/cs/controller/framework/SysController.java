0ef5f402c049dfabaad43d94e078d9e097a5f47e
cx <cx>
2017-07-27 19:41:10 +0800

    配置环境

--- 02.编码/政府投资项目管理系统/src/main/java/cs/controller/framework/SysController.java
+++ 02.编码/政府投资项目管理系统/src/main/java/cs/controller/framework/SysController.java
@@ -65,22 +65,30 @@ public class SysController {
 	
 	@RequestMapping(name = "系统初始化", path = "init", method = RequestMethod.GET)
 	public @ResponseBody String init(HttpServletRequest request) {
-		Response response = sysService.SysInit();
-		if(response.isSuccess()){
-			return "Init system success";
+		if(env.equals("dubug")){
+			Response response = sysService.SysInit();
+			if(response.isSuccess()){
+				return "Init system success";
+			}else{
+				return "Init system fail";
+			}				
 		}else{
-			return "Init system fail";
-		}				
+			return null;
+		}
 	}
 	
 	@RequestMapping(name = "基础数据初始化", path = "initBasicData", method = RequestMethod.GET)
 	public @ResponseBody String initBasicData(HttpServletRequest request) {
-		Response response = sysService.SysInitBasicData();
-		if(response.isSuccess()){
-			return "Init basicData success";
+		if(env.equals("dubug")){
+			Response response = sysService.SysInitBasicData();
+			if(response.isSuccess()){
+				return "Init basicData success";
+			}else{
+				return "Init basicData fail";
+			}	
 		}else{
-			return "Init basicData fail";
-		}	
+			return null;
+		}
 	}
 	//begin#html
 	@RequiresPermissions("sys#html/index#get")
