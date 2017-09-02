1bbe87b21e8157d62c9233e056d21b64de9de4e8
cx <daobantutu@foxmail.com>
2017-09-02 14:10:16 +0800

    申报信息审批页面

--- 02.编码/政府投资项目管理系统/src/main/webapp/contents/admin/management/task/audit/task.audit.svc.js
+++ 02.编码/政府投资项目管理系统/src/main/webapp/contents/admin/management/task/audit/task.audit.svc.js
@@ -8,12 +8,49 @@
 	function taskAudit($http,$location) {
 		var url_taskAudit = "/management/task/audit";
 		var url_shenbao = "/management/shenbao";
+		var url_replyFile = "/management/replyFile";
 		var service = {
 			grid : grid,//待办任务列表
 			getTaskInfoById:getTaskInfoById,//查询任务信息
-			getShenBaoInfoById:getShenBaoInfoById//查询申报信息
+			getShenBaoInfoById:getShenBaoInfoById,//查询申报信息
+			replyFileGird:replyFileGird,//批复文件库列表
+			saveShenBaoInfo:saveShenBaoInfo//保存申报信息
 		};
 		return service;
+		
+		/**
+		 * 保存申报信息
+		 */
+		function saveShenBaoInfo(vm){
+			var httpOptions = {
+					method : 'put',
+					url : url_shenbao,
+					data:vm.model.shenBaoInfo
+				};
+			
+			var httpSuccess = function success(response) {
+				common.requestSuccess({
+					vm:vm,
+					response:response,
+					fn:function(){
+						common.alert({
+							vm:vm,
+							msg:"保存成功！",
+							fn:function(){
+								$('.alertDialog').modal('hide');
+							}
+						});
+					}
+				});
+			};
+				
+			common.http({
+				vm:vm,
+				$http:$http,
+				httpOptions:httpOptions,
+				success:httpSuccess
+			});
+		}
 		
 		/**
 		 * 查询任务信息
@@ -48,7 +85,7 @@
 		function getShenBaoInfoById(vm){
 			var httpOptions = {
 					method : 'get',
-					url : common.format(url_shenbao + "?$filter=id eq '{0}'", vm.taskId)
+					url : common.format(url_shenbao + "?$filter=id eq '{0}'", vm.relId)
 				};
 			
 			var httpSuccess = function success(response) {
@@ -57,6 +94,63 @@
 					response:response,
 					fn:function(){
 						vm.model.shenBaoInfo = response.data.value[0] || {};
+						//数据的展示处理
+						//项目类型
+						vm.projectTypes = common.stringToArray(vm.model.shenBaoInfo.projectType,",");
+						//判断投资类型
+						if(vm.model.shenBaoInfo.projectInvestmentType == common.basicDataConfig().projectInvestmentType_SH){//社会投资
+							vm.isSHInvestment = true;
+							//基础数据--项目分类
+							vm.basicData.projectClassify=$linq(common.getBasicData())
+		    	       		.where(function(x){return x.identity==common.basicDataConfig().projectClassify&&x.pId==common.basicDataConfig().projectClassify_SH;})
+		    	       		.toArray();
+							//基础数据--行业归口
+							 vm.basicData.projectIndustry=$linq(common.getBasicData())
+			    	       		.where(function(x){return x.identity==common.basicDataConfig().projectIndustry&&x.pId==common.basicDataConfig().projectIndustry_ZF;})
+			    	       		.toArray();
+							 vm.projectIndustryChange=function(){    		
+				    	       		vm.basicData.projectIndustryChildren=$linq(common.getBasicData())
+				    	       		.where(function(x){return x.identity==common.basicDataConfig().projectIndustry&&x.pId==vm.model.projectIndustryParent;})
+				    	       		.toArray();
+			     			   };
+						}else if(vm.model.shenBaoInfo.projectInvestmentType == common.basicDataConfig().projectInvestmentType_ZF){//政府投资
+							vm.isZFInvestment = true;
+							//基础数据--项目分类
+							vm.basicData.projectClassify=$linq(common.getBasicData())
+		    	       		.where(function(x){return x.identity==common.basicDataConfig().projectClassify&&x.pId==common.basicDataConfig().projectClassify_ZF;})
+		    	       		.toArray();
+							//基础数据--行业归口
+		        		   vm.basicData.projectIndustry=$linq(common.getBasicData())
+		    	       		.where(function(x){return x.identity==common.basicDataConfig().projectIndustry&&x.pId==common.basicDataConfig().projectIndustry_ZF;})
+		    	       		.toArray();
+						}
+						//判断申报阶段
+						if(vm.model.shenBaoInfo.projectShenBaoStage ==common.basicDataConfig().projectShenBaoStage_projectProposal){//申报阶段为:项目建议书
+							vm.isProjectProposal=true;
+							vm.materialsType=common.uploadFileTypeConfig().projectShenBaoStage_projectProposal;
+		    			    vm.uploadType=[['JYS','项目建议书'],['KXXYJBG','可行性研究报告'],['CBSJYGS','初步设计与概算']];
+						}else if(vm.model.shenBaoInfo.projectShenBaoStage == common.basicDataConfig().projectShenBaoStage_KXXYJBG){//申报阶段为:可行性研究报告
+							vm.isKXXYJBG=true;
+							vm.materialsType=common.uploadFileTypeConfig().projectShenBaoStage_KXXYJBG;
+		    			    vm.uploadType=[['JYS','项目建议书'],['KXXYJBG','可行性研究报告'],['CBSJYGS','初步设计与概算']];
+						}else if(vm.model.shenBaoInfo.projectShenBaoStage == common.uploadFileTypeConfig().projectShenBaoStage_CBSJYGS){//申报阶段为:初步设计与概算
+							vm.isCBSJYGS=true;
+							vm.materialsType=common.uploadFileTypeConfig().projectShenBaoStage_CBSJYGS;
+		    			    vm.uploadType=[['JYS','项目建议书'],['KXXYJBG','可行性研究报告'],['CBSJYGS','初步设计与概算']];
+						}
+						//时间的显示
+						vm.model.shenBaoInfo.beginDate=common.formatDate(vm.model.shenBaoInfo.beginDate);//开工日期
+						vm.model.shenBaoInfo.endDate=common.formatDate(vm.model.shenBaoInfo.endDate);//竣工日期
+						vm.model.shenBaoInfo.pifuJYS_date=common.formatDate(vm.model.shenBaoInfo.pifuJYS_date);//项目建议书批复日期			
+						vm.model.shenBaoInfo.pifuKXXYJBG_date=common.formatDate(vm.model.shenBaoInfo.pifuKXXYJBG_date);//可行性研究报告批复日期
+						vm.model.shenBaoInfo.pifuCBSJYGS_date=common.formatDate(vm.model.shenBaoInfo.pifuCBSJYGS_date);//初步设计与概算批复日期
+						//资金计算显示
+						//计算资金筹措总计
+						vm.capitalTotal=function(){
+				  			 return common.getSum([vm.model.shenBaoInfo.capitalSCZ_ggys||0,vm.model.shenBaoInfo.capitalSCZ_gtzj||0,vm.model.shenBaoInfo.capitalSCZ_zxzj||0,
+  			 						vm.model.shenBaoInfo.capitalQCZ_ggys||0,vm.model.shenBaoInfo.capitalQCZ_gtzj||0,
+  			 						vm.model.shenBaoInfo.capitalSHTZ||0,vm.model.shenBaoInfo.capitalZYYS||0,vm.model.shenBaoInfo.capitalOther||0]);
+				  		 };
 					}
 				});
 			};
@@ -69,7 +163,71 @@
 			});
 		}
 		
-		// begin#grid
+		/**
+		 * 批复文件列表
+		 */
+		function replyFileGird(vm){
+			// Begin:dataSource
+			var dataSource = new kendo.data.DataSource({
+				type : 'odata',
+				transport : common.kendoGridConfig().transport(url_replyFile),						
+				schema : common.kendoGridConfig().schema({
+					id : "id"
+				}),
+				serverPaging : true,
+				serverSorting : true,
+				serverFiltering : true,
+				pageSize : 10
+					
+			});
+			// End:dataSource
+			// Begin:column
+			var columns = [
+				{
+					template : function(item) {
+						return kendo
+								.format(
+										"<input type='radio'  relId='{0},{1},{2}' name='checkbox'/>",
+										item.number,item.name,item.fullName);
+					},
+					filterable : false,
+					width : 40,
+					title : ""
+				},
+				{
+					field : "number",
+					title : "文号",
+					width:180,
+					
+					filterable : true
+				},
+				{
+					field : "name",
+					title : "文件名",
+					width : 450,
+					template:function(item){
+						return common.format("<a href='/contents/upload/{1}'>{0}</a>",item.name,item.fullName);
+					},
+					filterable : true
+				}
+			];
+			// End:column
+
+			vm.replyFileGridOptions = {
+				dataSource : common.gridDataSource(dataSource),
+				filterable : common.kendoGridConfig().filterable,
+				pageable : common.kendoGridConfig().pageable,
+				noRecords : common.kendoGridConfig().noRecordMessage,
+				columns : columns,
+				resizable : true
+			};
+		
+		}
+		
+		
+		/**
+		 * 个人待办列表
+		 */
 		function grid(vm) {
 			// Begin:dataSource
 			var dataSource = new kendo.data.DataSource({
