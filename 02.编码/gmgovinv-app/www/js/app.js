98098df8610ff4b940b2ccc210359ad1f4fa8944
qsz <397926069@qq.com>
2018-04-03 17:28:35 +0800

    将手机端的请求改为https方式，更换新的IP和端口

--- 02.编码/gmgovinv-app/www/js/app.js
+++ 02.编码/gmgovinv-app/www/js/app.js
@@ -12,7 +12,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','s
     app_name: '光明政府投资项目',
     platform: 'android',
     version: '1.0.0',
-    host: 'http://120.77.208.59:8080'
+    host: 'https://192.168.189.235:8443'
   })
 
   .constant('APP_EVENTS', {
