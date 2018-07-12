b5c1a0d318394af11ce15eaef519f6dcb2d7a022
tzg <tang_guo_168@163.com>
2018-07-12 10:56:34 +0800

    调整全局错误处理

--- 02.编码/政府投资项目管理系统/src/main/java/cs/common/GlobalDefaultExceptionHandler.java
+++ 02.编码/政府投资项目管理系统/src/main/java/cs/common/GlobalDefaultExceptionHandler.java
@@ -3,6 +3,7 @@ package cs.common;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,54 +14,59 @@ import cs.service.framework.RoleServiceImpl;
 
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.UnauthenticatedException;
+
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
-	private static Logger logger = Logger.getLogger(GlobalDefaultExceptionHandler.class);
-	
-	public static final String DEFAULT_ERROR_VIEW = "error";
+    private static Logger logger = Logger.getLogger(GlobalDefaultExceptionHandler.class);
 
-	@ExceptionHandler(value = IllegalArgumentException.class)
-	public @ResponseBody Response illegalErrorHandler(HttpServletRequest req, Exception e) throws Exception {
-		Response response = new Response();
-		response.setMessage(e.getMessage());
-		response.setStatus(555);
-		logger.warn( e.getMessage());
-		return response;
-	}
-	
-	@ExceptionHandler(value ={ UnauthenticatedException.class,AuthorizationException.class})
-	@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
-	public String unAuthErrorHandler(HttpServletRequest req, Exception e) throws Exception {			
-		logger.warn("无权限访问");
-		//获取当前的请求url
-		String url = req.getServletPath();
-		String[] urls = url.split("\\/");
-		if(urls[1].equals("management")){
-			return "forward:/adminLogin";
-		}else{
-			return "forward:/";
-		}
-	}
-	@ExceptionHandler(value = Exception.class)
-	public void  errorHandler(HttpServletRequest req, Exception e) throws Exception {
-		
-		logger.error("通用错误处理", e.getCause());
-		e.printStackTrace();
-		throw e;		
-		
-		// If the exception is annotated with @ResponseStatus rethrow it and let
-		// the framework handle it - like the OrderNotFoundException example
-		// at the start of this post.
-		// AnnotationUtils is a Spring Framework utility class.
-		// if (AnnotationUtils.findAnnotation(e.getClass(),
-		// ResponseStatus.class) != null)
-		// throw e;
+    public static final String DEFAULT_ERROR_VIEW = "error";
 
-		// // Otherwise setup and send the user to a default error-view.
-		// ModelAndView mav = new ModelAndView();
-		// mav.addObject("exception", e);
-		// mav.addObject("url", req.getRequestURL());
-		// mav.setViewName(DEFAULT_ERROR_VIEW);
-		// return mav;
-	}
+    @Autowired
+    private HttpServletRequest request;
+
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    @ResponseBody
+    public Response illegalErrorHandler(IllegalArgumentException e) {
+        logger.error("错误处理", e.getCause());
+        Response response = new Response();
+        response.setMessage(e.getMessage());
+        response.setStatus(555);
+        return response;
+    }
+
+    @ExceptionHandler(value = {UnauthenticatedException.class, AuthorizationException.class})
+    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+    public String unAuthErrorHandler(Exception e) {
+        logger.error("无权限访问", e.getCause());
+        //获取当前的请求url
+        String url = request.getServletPath();
+        String[] urls = url.split("\\/");
+        if (urls[1].equals("management")) {
+            return "forward:/adminLogin";
+        } else {
+            return "forward:/";
+        }
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public void errorHandler(HttpServletRequest req, Exception e) throws Exception {
+        logger.error("通用错误处理", e.getCause());
+        e.printStackTrace();
+        throw e;
+
+        // If the exception is annotated with @ResponseStatus rethrow it and let
+        // the framework handle it - like the OrderNotFoundException example
+        // at the start of this post.
+        // AnnotationUtils is a Spring Framework utility class.
+        // if (AnnotationUtils.findAnnotation(e.getClass(),
+        // ResponseStatus.class) != null)
+        // throw e;
+
+        // // Otherwise setup and send the user to a default error-view.
+        // ModelAndView mav = new ModelAndView();
+        // mav.addObject("exception", e);
+        // mav.addObject("url", req.getRequestURL());
+        // mav.setViewName(DEFAULT_ERROR_VIEW);
+        // return mav;
+    }
 }
