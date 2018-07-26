90dcdefceb9888889b7bacd4fd676606f2165b24
tzg <tang_guo_168@163.com>
2018-07-26 17:10:30 +0800

    调整 bsWin 的样式

--- 02.编码/政府投资项目管理系统/src/main/webapp/contents/common/common.bsWin.svc.js
+++ 02.编码/政府投资项目管理系统/src/main/webapp/contents/common/common.bsWin.svc.js
@@ -194,11 +194,8 @@
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>\
                             <h4 class="modal-title text-info">{{title || \'消息提醒\'}}</h4>\
                         </div>\
-                        <div class="modal-body text-{{type==\'error\'? \'danger\' : type}}">\
-                            <p class="alertDialogMessage">\
-                                <i class="fa fa-{{type == \'success\' ? \'check\' : (type == \'error\' ? \'ban\' : type)}}" aria-hidden="true"></i> \
-                                {{message}}\
-                            </p>\
+                        <div class="modal-body text-danger">\
+                            <p class="alertDialogMessage">{{message}}</p>\
                         </div>\
                         <div class="modal-footer">\
                             <button type="button"  class="btn btn-btn-default btn-sm" data-dismiss="modal">关闭</button>\
