7cae8188c8a50c9a90f5a436aae61903dcdd4aa9
liux <443186430@qq.com>
2018-08-17 18:44:04 +0800

    修改审批在线编辑功能

--- 02.编码/政府投资项目管理系统/src/main/java/cs/common/SNKit.java
+++ 02.编码/政府投资项目管理系统/src/main/java/cs/common/SNKit.java
@@ -1,16 +1,20 @@
 package cs.common;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.sn.framework.common.SnRuntimeException;
 import com.sn.framework.common.StringUtil;
 import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.util.Assert;
+import org.springframework.util.FileCopyUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
+import static com.sn.framework.common.StringUtil.GBK;
+import static com.sn.framework.common.StringUtil.ISO_8859_1;
 import static com.sn.framework.common.StringUtil.UTF_8;
 
 /**
@@ -96,5 +100,82 @@ public class SNKit {
             }
         }
     }
+
+    /**
+     * 获取系统基础路径
+     *
+     * @param request
+     * @return
+     */
+    public static String getBasePath(HttpServletRequest request) {
+        String scheme = request.getScheme();
+        String serverName = request.getServerName();
+        int port = request.getServerPort();
+        String path = request.getContextPath();
+        if (port != 80) {
+            path = ":" + port + path;
+        }
+        return scheme + "://" + serverName + path;
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param response
+     * @param inputStream
+     */
+    public static void fileDownload(HttpServletResponse response, InputStream inputStream) {
+        try {
+            FileCopyUtils.copy(inputStream, response.getOutputStream());
+
+        } catch (IOException e) {
+//            e.printStackTrace();
+            throw new SnRuntimeException("文件下载失败", e.getCause());
+        }
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param response
+     * @param outFile
+     */
+    public static void fileDownload(HttpServletResponse response, File outFile) {
+        Assert.notNull(outFile, "缺少参数");
+        if (!outFile.exists()) {
+            logger.warn("硬盘上不存在文件 {}"+outFile.getName());
+            throw new SnRuntimeException(String.format("找不到【%s】文件", outFile.getName()));
+        }
+        response.addHeader("Content-Length", "" + outFile.length());  //返回头 文件大小
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(outFile);
+        } catch (FileNotFoundException e) {
+            logger.error("文件不存在 {}", e);
+        }
+        fileDownload(response, inputStream);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param response     请求响应对象
+     * @param outFile      下载的文件对象
+     * @param originalName 文件原始名称
+     * @throws IOException
+     */
+    public static void fileDownload(HttpServletRequest request, HttpServletResponse response, File outFile, String originalName) {
+        response.setCharacterEncoding(UTF_8.name());
+        response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
+        String filename = null, userAgent = request.getHeader("user-agent");
+        if (userAgent.indexOf("MSIE") != -1 || userAgent.indexOf("Trident") != -1 || userAgent.indexOf("Edge") != -1) {     // ie
+            filename = new String(originalName.getBytes(GBK), ISO_8859_1);
+        } else {
+            filename = new String(originalName.getBytes(UTF_8), ISO_8859_1);
+        }
+        response.setHeader("Content-disposition", "attachment;filename=" + filename);
+        fileDownload(response, outFile);
+    }
+
 
 }
