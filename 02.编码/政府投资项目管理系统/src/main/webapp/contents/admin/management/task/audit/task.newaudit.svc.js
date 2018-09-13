f6b5c0e83f2b897cecce4575d451e01d6c119134
wcq <wagchaoqi1992@163.com>
2018-09-13 09:29:51 +0800

    发文页面调整

--- 02.编码/政府投资项目管理系统/src/main/webapp/contents/admin/management/task/audit/task.newaudit.svc.js
+++ 02.编码/政府投资项目管理系统/src/main/webapp/contents/admin/management/task/audit/task.newaudit.svc.js
@@ -295,11 +295,6 @@
                         if (vm.draft.id) {
                             vm.draft.draftDate = common.formatDate(vm.draft.draftDate);//开工日期
                             vm.userNameAndUnitName = vm.getUserName(vm.draft.userNameAndUnit);
-                            if(vm.draft.drafts){
-                            	vm.draft.drafts = "是";
-                            }else{
-                            	vm.draft.drafts = "否";
-                            }
                         } else {
                             //初始化相关数据
                             vm.draft.draftDate = common.formatDate(new Date());
