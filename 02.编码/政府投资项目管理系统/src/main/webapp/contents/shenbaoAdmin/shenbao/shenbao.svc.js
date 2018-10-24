6fe8470dc8fecb5a3f3048a5e2a84b6ee747c1a2
liux <443186430@qq.com>
2018-10-24 17:45:26 +0800

    生产数据库里面申报表所有的下一年度计划申请信息的开工竣工日期都改成年月，其他概算，立项等信息不变。

--- 02.编码/政府投资项目管理系统/src/main/webapp/contents/shenbaoAdmin/shenbao/shenbao.svc.js
+++ 02.编码/政府投资项目管理系统/src/main/webapp/contents/shenbaoAdmin/shenbao/shenbao.svc.js
@@ -743,8 +743,8 @@
 						vm.canDelete = false;
 					}
 					//日期展示
-					//vm.model.beginDate=common.formatDate(vm.model.beginDate);//开工日期
-					//vm.model.endDate=common.formatDate(vm.model.endDate);//竣工日期
+					//vm.model.beginDate=common.formatDateByStr(vm.model.beginDate,"yyyy-MM");//开工日期
+					//vm.model.endDate=common.formatDateByStr(vm.model.endDate,"yyyy-MM");//竣工日期
 					vm.model.pifuJYS_date=common.formatDate(vm.model.pifuJYS_date);//项目建议书批复日期			
 					vm.model.pifuKXXYJBG_date=common.formatDate(vm.model.pifuKXXYJBG_date);//可行性研究报告批复日期
 					vm.model.pifuCBSJYGS_date=common.formatDate(vm.model.pifuCBSJYGS_date);//初步设计与概算批复日期						
@@ -905,9 +905,11 @@
 				if(vm.page=='edit'){//如果为申报信息填写页面
 					vm.model.projectType = common.stringToArray(vm.model.projectType,',');//多选框回显	
 					//日期展示处理
-					//vm.model.beginDate=common.formatDate(vm.model.beginDate);//开工日期
-					//vm.model.endDate=common.formatDate(vm.model.endDate);//竣工日期
-					vm.model.pifuJYS_date=common.formatDate(vm.model.pifuJYS_date);//项目建议书批复日期			
+					if(vm.isYearPlan){
+                        vm.model.beginDate=common.formatDateByStr(vm.model.beginDate,"yyyy-MM");//开工日期
+                        vm.model.endDate=common.formatDateByStr(vm.model.endDate,"yyyy-MM");//竣工日期
+                    }
+					vm.model.pifuJYS_date=common.formatDate(vm.model.pifuJYS_date);//项目建议书批复日期
 					vm.model.pifuKXXYJBG_date=common.formatDate(vm.model.pifuKXXYJBG_date);//可行性研究报告批复日期
 					vm.model.pifuCBSJYGS_date=common.formatDate(vm.model.pifuCBSJYGS_date);//初步设计与概算批复日期 					
 					
